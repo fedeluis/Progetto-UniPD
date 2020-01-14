cd5cd81b2051ace2af575b56fa381de850faba27
FranPago99 <32836865+FranPago99@users.noreply.github.com>
2020-01-14 14:54:22 +0100

    Add files via upload

--- Componente.h
+++ Componente.h
@@ -1,19 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 class Componente{
 	private:
 		int component_id; //id del componente
-		std::string component_name; //nome del componente
+		string component_name; //nome del componente
 		int delivery_time; //tempo di spedizione
-		//double component_price; //prezzo del componente
+		int component_price; //prezzo del componente
 		
 	public:
-		Componente(int id,std::string name,int time/*,double price*/); //costruttore
+		Componente(int id,string name,int time,int price); //costruttore
 		
 		void print(); //stampa componente
 		
 		int getComponent_id(); //ritorna l'id del componente
-		std::string getComponent_name(); //ritorna il nome del componente
+		string getComponent_name(); //ritorna il nome del componente
 		int getDelivery_time(); //ritorna il tempo di spedizione del componente
-		//double getComponent_price(); //ritorna il prezzo del componente
+		int getComponent_price(); //ritorna il prezzo del componente
 };
