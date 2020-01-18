3d56f4de2243fd705cd9bbaaf305a6a99cf78ba4
fede_luis <51258845+fedeluis@users.noreply.github.com>
2020-01-18 11:03:19 +0100

    Add files via upload

---
+++ System.h
@@ -0,0 +1,81 @@
+// Federico Luisetto    1187521
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class System {
+
+    public:
+        /**
+         * errori
+         */
+        class no_such_component{};
+        /**
+         * costruttore
+         */
+        System();
+        /**
+         * inserisce nuovi componenti ordinati
+         * ordinamento in base al mese
+         * in: (component_id, quantity, month)
+         */
+        void add_c_shop(int,int,int);
+        /**
+         * inserisce nuovi componenti in magazzino
+         * quando arrivano i componenti si aggiungono automaticamente
+         * in: (component_id, quantity)
+         */
+        void add_c_stock(int, int);
+        /**
+         * inserisce in lista gli ordini evasi
+         * in: (model_name, quantity)
+         */
+        void add_m_sold(string, int);
+        /**
+         * toglie un mese ad ogni componente della lista c_ord
+         */
+        void add_month();
+        /**
+         * toglie i componenti necessari alla produzione dallo stock
+         * in: (component_id, quantity)
+         */
+        void catch_from_stock(int, int);
+        /**
+         * stampa lo stato corrente quando un ordine e' evaso
+         * out: (c_ord, c_stock, m_done)
+         */
+        void print_current_status();
+
+        //definizione strutture
+        /**
+         * componenti ordinati e non ancora arrivati
+         * month indica il mese di arrivo dei componenti
+         */
+        typedef struct {
+            int component_id;
+            int quantity;
+            int month;
+        } shop_done;
+        /**
+         * componenti arrivati in magazzino ma non ancora usati
+         * quantity e' quantita' totale di un componente
+         */
+        typedef struct {
+            int component_id;
+            int quantity;
+        } stock;
+        /**
+         * ordini evasi
+         */
+        typedef struct {
+            string model_name;
+            int quantity;
+        } order_sold;
+
+    private:
+        list<shop_done> c_ord;
+        list<stock> c_stock;
+        list<order_sold> m_done;
+};
