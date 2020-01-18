b1481d9fef45a263b3390b940ce2705fde2e5048
FranPago99 <32836865+FranPago99@users.noreply.github.com>
2020-01-18 11:33:42 +0100

    Add files via upload

--- Money.h
+++ Money.h
@@ -1,10 +1,17 @@
+//Pagotto Francesco 1195886
+
 #include <iostream>
 
 class Money{
 	public:
-		getMoney(); //ritorna quanti fondi sono disponibili
-		buyComponent(int); //viene passato l'id del componente e se i fondi sono disponibili acquista il componente e vengono scalati i soldi
-		sellModel(int); //viene passato l'id del model che viene venduto e viene guadagnato il prezzo di vendita 
+		class notEnoughMoney();
+		
+		Money();
+		Money(int); //soldi iniziali
+		
+		int getMoney(); //ritorna quanti fondi sono disponibili
+		void buyComponent(int); //viene passato l'id del componente e se i fondi sono disponibili acquista il componente e vengono scalati i soldi
+		void sellModel(int); //viene passato l'id del model che viene venduto e viene guadagnato il prezzo di vendita 
 	
 	private:
 		int money;
