257de82b9bc2d2cbeab6b9d20d8f418d8bcac78c
fede_luis <51258845+fedeluis@users.noreply.github.com>
2020-01-28 22:13:25 +0100

    add scan_files()

--- main.cpp
+++ main.cpp
@@ -1,10 +1,96 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 #include "System.h"
 #include "Money.h"
 #include "Orders.h"
 #include "Model.h"
 #include "Component.h"
 using namespace std;
+
+//funzione per leggere model
+list<Model> scan_files() {
+    list<Model> output; //contiene tutti gli elettrodomestici
+    vector<string> nomi;    //contiene nomi file
+    /**
+     * lettura file
+     */
+    string reader;
+    ifstream file{"files/models.dat"}; //apre il file models.dat
+	
+    if(!file) 
+        throw Model::invalid_file();
+
+    //legge ogni riga e la salva
+    do {
+        file>>reader;
+        nomi.push_back("files/"+reader);
+        getline(file,reader);
+    } while(file);
+
+    // chiude il file models.dat
+    file.close();
+
+
+    /**
+     * leggo ed inserisco modelli
+     */
+    for(int index=0; index<nomi.size(); index++) {
+        Model mod;
+        int id, qnt;    //componente
+        string name;    //componente
+        /**
+        * lettura file
+        */
+        ifstream file{nomi[index]}; //apre il file salvato in nomi[]
+
+        if(!file)
+            throw Model::invalid_file();
+
+        bool first_row = true; //differenzia lettura righe
+
+        /* legge e stampa ogni riga */
+    
+        do {
+            //id
+            file>>reader;
+            if(first_row)
+                mod.set_id(stoi(reader));
+            else
+                id = stoi(reader);
+            
+            //name
+            file>>reader;
+            if(first_row)
+                mod.set_name(reader);
+            else
+                name = reader;
+
+            //price or quantity needed
+            file>>reader;
+            if(first_row)
+                mod.set_price(stoi(reader));
+            else
+                qnt = stoi(reader);
+            
+            if(first_row)   //dopo una lettura first_row=false
+                first_row = false;
+            else    //da seconda riga inserisco componenti
+                mod.add_component(id,name,qnt);
+
+            getline(file,reader);
+        } while(file);
+
+        // chiudo il file di indice index
+        file.close();
+
+        //inserisco nella lista modello letto
+        output.push_back(mod);
+    }
+
+    return output;
+}
+
 Orders ScrollOrders(list<Orders> &NewOrders)
 {
 	Orders ord;
@@ -67,7 +153,7 @@ int main()
 	list<Orders> NewOrders;
 	list<Component> allComponents;
 	list<Component> components;
-	list<Model> models;
+	list<Model> models = scan_files();
 	Component currentComponent;
 	Orders temp;
 	int length;
